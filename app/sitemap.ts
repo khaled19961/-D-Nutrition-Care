c8a40a76c5e828cf7605c6230961acbfe8d6a4ca
khaled19961 <akhtyarathamnasbat@gmail.com>
2026-09-26 12:56:30 +0300

    seo: add storefront navigation routes to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@ import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
-  const routes = ["/", "/nutritionists", "/programs", "/articles", "/about", "/booking"];
+  const routes = ["/", "/categories", "/offers", "/more", "/nutritionists", "/programs", "/articles", "/about", "/booking"];
   return routes.map((route) => ({
     url: `${siteUrl}${route}`,
     lastModified: new Date(),
